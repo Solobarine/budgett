e990effe610a93bae7c5f232c74a21be118e1d71
Solomon <solobarine@gmail.com>
2023-06-08 16:27:25 +0100

    Create routes for the App

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources 'group', only: ['create', 'new', 'show', 'index'] do
+    resources 'deal', only: ['create', 'new']
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "splash#index"
 end
