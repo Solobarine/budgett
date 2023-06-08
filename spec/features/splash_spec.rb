545d69560bd0146101494203e29ffc9e4c820325
Solomon <solobarine@gmail.com>
2023-06-08 15:50:08 +0100

    Create tests for project

---
+++ spec/features/splash_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Group, model: :feature do
+    before :each do
+        visit root_path
+    end
+
+    it 'Show the Splash Page' do
+        expect(page).to have_content('Budgett')
+        expect(page).to have_link('Register')
+        expect(page).to have_link('Login')
+    end
+
+    it 'redirects to the login page' do
+        click_link 'Login'
+        expect(page).to have_current_path(new_user_session_path)
+        expect(page).to have_field('Email')
+        expect(page).to have_field('Password')
+    end
+    
+    it 'redirects to the signup page' do
+        click_link 'Register'
+        expect(page).to have_current_path(new_user_registration_path)
+        expect(page).to have_field('Name')
+        expect(page).to have_field('Email')
+        expect(page).to have_field('Password')
+    end
+end
---
+++ spec/models/deal_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Deal, type: :model do
+    before(:each) do
+      user = User.new(name: 'Peter', email: 'peter@gmail.com', password: '123456', password_confirmation: '123456')
+      user.save
+      @group = Group.create(name: 'Food', icon:'blob', user_id: user.id)
+      @deal = Deal.new(name: 'Apples', amount: 23.45, user_id: user.id)
+    end
+  
+    it 'should have a name' do
+      @deal.name = nil
+      @deal.save
+      expect(@deal).to_not be_valid
+    end
+  
+    it 'should have an amount in the form of a number' do
+      @deal.amount = nil
+      @deal.save
+      expect(@deal).to_not be_valid
+      @deal.amount = 'abc'
+      @deal.save
+      expect(@deal).to_not be_valid
+    end
+  end
